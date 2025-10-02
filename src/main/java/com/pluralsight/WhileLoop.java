5f42f8398fc6239a7644af6f77e1b7ed1a35cb28
heavenrose <heavenrose9515@gmail.com>
2025-10-02 10:27:22 -0400

    first draft

---
+++ src/main/java/com/pluralsight/WhileLoop.java
@@ -0,0 +1,14 @@
+package com.pluralsight;
+
+public class WhileLoop {
+    public static void main(String[] args) {
+         //Step 1 declare the variable
+        int Spongebob = 0; //step 1: Start at 0
+        while (Spongebob < 5) {  //Step2: Loop while less than r
+            System.out.println("I love Java"); //step 3 : action
+            Spongebob = Spongebob +=1; //step 4: update counter
+        }
+
+
+    }
+}
