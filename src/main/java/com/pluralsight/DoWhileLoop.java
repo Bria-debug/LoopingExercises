4b0607adaf657f6376886d72cfcd0fb00a305835
heavenrose <heavenrose9515@gmail.com>
2025-10-02 11:17:39 -0400

    first draft

---
+++ src/main/java/com/pluralsight/DoWhileLoop.java
@@ -0,0 +1,12 @@
+package com.pluralsight;
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        int Sandy = 0;
+        do {
+            System.out.println("I love Java");//action
+            Sandy = Sandy += 1;
+        } while (Sandy <5);
+
+    }
+}
